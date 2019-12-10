404ed77680c4cde18b10fb480b32618155cfd102
root <root@vultr.guest>
2019-12-10 12:53:35 +0000

    test with data oriëented programming

---
+++ main.js
@@ -0,0 +1,22 @@
+class ParticleClass {
+	constructor(){
+		this.data_oriented = true
+		this.numParticlesPerSec = 20,
+		this.oopData = {
+			particleProperties: {},
+			environmentProperties: {},
+		}
+		this.dodData = {
+			emitter: [],
+			environment: [],
+			particles: [],
+		}
+	}
+	generateOopData(){}
+	generateDodData(){
+		// emitter
+		emitter.push(this.numParticlesPerSec) // birthRate
+
+	}
+
+}
